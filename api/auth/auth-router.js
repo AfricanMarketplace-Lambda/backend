3eab8dd7f37e8d7176aebdf153a7d97ed3b1d7f5
Samuel Lee <samuelxlee@gmail.com>
2021-01-31 15:30:10 -0800

    wrote endpoint for logging in owners and function to generate the token

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,6 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../../config/secrets");
 const router = require("express").Router();
 
 const Owner = require("../owners/owners-model");
@@ -25,3 +27,35 @@ router.post("/register", checkRegisterRequest, checkUniqueUsername, (req, res) =
             res.status(500).json({ message: "Something wrong happened!" });
         });
 });
+
+router.post("/login", checkLoginRequest, checkUsernameExists, (req, res) => {
+    const { password } = req.body;
+    try {
+        const verifyUser = bcrypt.compareSync(password, req.ownerData.password);
+        if (verifyUser) {
+            const token = generateToken(req.ownerData);
+            res.status(200).json({
+                message: "Welcome to our API" + req.ownerData.username,
+                token
+            });
+        } else {
+            res.status(401).json({ message: "You are not allowed in here!" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Something wrong happened!" });
+    }
+});
+
+function generateToken(owner) {
+    const payload = {
+        subject: owner.id,
+        username: owner.username
+    }
+    const options = {
+        expiresIn: "1d"
+    }
+
+    return jwt.sign(payload, jwtSecret, options);
+}
+
+module.exports = router;
