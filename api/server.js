51e8258a39049f3dc105aa2fc70e340760e362ed
Samuel Lee <samuelxlee@gmail.com>
2021-02-05 12:22:30 -0800

    added restricted middleware for endpoints

--- api/server.js
+++ api/server.js
@@ -7,14 +7,15 @@ const server = express();
 const authRouter = require("./auth/auth-router");
 const ownerRouter = require("./owners/owners-router");
 const itemRouter = require("./items/items-router");
+const restricted = require("./auth/restricted");
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
 server.use("/api/auth", authRouter);
-server.use("/api/owners", ownerRouter);
-server.use("/api/items", itemRouter);
+server.use("/api/owners", restricted, ownerRouter);
+server.use("/api/items", restricted, itemRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "API is up and running" });
